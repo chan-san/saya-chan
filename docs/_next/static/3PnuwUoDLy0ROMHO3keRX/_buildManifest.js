773f0299aafe15ba6875dd9e846acdb8e1b75bfa
chan-san <10119281+chan-san@users.noreply.github.com>
2022-08-10 13:33:05 +0900

    Deploy Next.js to gh-pages

--- docs/_next/static/3PnuwUoDLy0ROMHO3keRX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2453a230a9d09809.js"],"/_error":["static/chunks/pages/_error-815e492bede44f3e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
